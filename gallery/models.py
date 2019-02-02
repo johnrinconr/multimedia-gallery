b61fc48cb8aa9d11174ea8a5d98b7b45c7402759
glpinto10 <gl.pinto10@uniandes.edu.co>
2019-02-02 15:23:11 -0500

    Image input is removed from test app

--- gallery/models.py
+++ gallery/models.py
@@ -9,7 +9,6 @@ class Image(models.Model):
     url = models.CharField(max_length=1000)
     description = models.CharField(max_length=1000, null=True)
     type = models.CharField(max_length=5, blank=True)
-    imageFile = models.ImageField(upload_to='images', null=True)
 
     def __str__(self):
         return 'Image: ' + self.name
@@ -18,4 +17,4 @@ class Image(models.Model):
 class ImageForm(ModelForm):
     class Meta:
         model = Image
-        fields = ['name', 'url', 'description', 'type', 'imageFile']
+        fields = ['name', 'url', 'description', 'type']
