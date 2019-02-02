7fcfc4050a46e6373f413730070f3cd8152d390f
glpinto10 <gl.pinto10@uniandes.edu.co>
2019-02-02 15:13:31 -0500

    Initial app base

---
+++ multimediaGallery/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+
+admin.autodiscover()
+
+
+# To add a new path, first import the app:
+# import blog
+#
+# Then add the new path:
+# path('blog/', blog.urls, name="blog")
+#
+# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('gallery/', include('gallery.urls', namespace="images")),
+]
